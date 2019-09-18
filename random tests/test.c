74993aa9e8b5bee4d09a457b825dd179defcda59
stefanberg96 <bergshm@gmail.com>
2019-09-18 09:09:30 +0200

    fixed benchmarking issue where the instruction was still gotten from cache. Secure mul now take 61 cycles

--- random tests/test.c
+++ random tests/test.c
@@ -1,26 +1,28 @@
 #include <stdint.h>
 #include <stdio.h>
 
-extern uint64_t getcycles();
-extern uint32_t securemul();
-extern uint32_t add(uint32_t a, uint32_t b);
+extern uint32_t getcycles();
+extern uint32_t securemul(unsigned char a, unsigned char b);
+extern uint32_t add();
 extern uint32_t mul320(unsigned char a);
 
+uint32_t dobenchmark(){
+
+   unsigned char a = 11;
+   unsigned char b = 50;
+   uint32_t result;
+   uint32_t oldcount, newcount;
+
+   oldcount = getcycles();
+   result = securemul(a,b);
+   newcount = getcycles();
+   return newcount-oldcount;
+}
+
 int main() {
-  uint64_t oldcount, newcount;
-  uint32_t a = 10;
-  uint32_t b = 50;
-  uint32_t l;
-  securemul();
-  securemul();
-  getcycles();
-  securemul();
-  getcycles();
-  securemul();
-  oldcount = getcycles();
-  l=securemul();
-  newcount = getcycles();
-  printf("This took %llu cycles\n", newcount - oldcount);
-  printf("the result of %d * %d = %d == %d\n", a, b, l,l);
+  uint32_t oldcount, newcount, x;
+  x = dobenchmark();
+  x = dobenchmark();
+  printf("This took %u cycles\n",x);
   return 0;
 }
