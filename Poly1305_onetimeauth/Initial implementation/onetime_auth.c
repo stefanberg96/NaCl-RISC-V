7681b2605eb51aecd863cd13ba491c3acb7d2017
stefanberg96 <bergshm@gmail.com>
2019-09-20 18:51:42 +0200

    Broken mulmod version probably problem with the addressing and stack being overwritten

--- Poly1305_onetimeauth/Initial implementation/onetime_auth.c
+++ Poly1305_onetimeauth/Initial implementation/onetime_auth.c
@@ -1,10 +1,15 @@
 #include <stdint.h>
 #include <stdio.h>
 
-extern uint64_t getcycles();
+extern uint32_t getcycles();
 extern uint32_t securemul(unsigned char a, unsigned char b);
 extern uint32_t mul320(uint32_t a);
 extern void squeezeasm(unsigned int h[17]);
+extern void mulmodasm(unsigned int h[17], unsigned int r[17]);
+
+void aliveprint(){
+  printf("ITS ALIVE!!\n");
+}
 
 // add the two numbers together without reduction
 static void add(unsigned int h[17], const unsigned int c[17]) {
@@ -31,7 +36,7 @@ static void squeeze(unsigned int h[17]) {
   u += h[16];
   h[16] = u & 3;
   u >>= 2;
-  u = 5* u; 
+  u = 5 * u;
 
   for (j = 0; j < 16; ++j) {
     u += h[j];
@@ -78,7 +83,7 @@ static void mulmod(unsigned int h[17], const unsigned int r[17]) {
   }
   for (i = 0; i < 17; ++i)
     h[i] = hr[i];
-  squeeze(h);
+  squeezeasm(h);
 }
 
 // input is in little endian
@@ -88,7 +93,6 @@ int crypto_onetimeauth(unsigned char *out, const unsigned char *in,
   unsigned int r[17];
   unsigned int h[17];
   unsigned int c[17];
-
   // create R from the first 16 bytes of the key
   r[0] = k[0];
   r[1] = k[1];
@@ -119,9 +123,11 @@ int crypto_onetimeauth(unsigned char *out, const unsigned char *in,
       c[j] = in[j]; // fill c with a chunk of 16 bytes from the in param
     c[j] = 1;       // append the 1 byte to the chunk
     in += j;
-    inlen -= j;   // update loop variants (inlen and increment in pointer)
-    add(h, c);    // c to the state
-    mulmod(h, r); // multiply state with the secret key modulo 2^130-5
+    inlen -= j; // update loop variants (inlen and increment in pointer)
+    add(h, c);  // c to the state
+    printf("Still alive??\n");
+    mulmodasm(h, r); // multiply state with the secret key modulo 2^130-5
+    printf("Still got here\n");
   }
 
   freeze(h); // calculate mod 2^130-5
@@ -135,6 +141,15 @@ int crypto_onetimeauth(unsigned char *out, const unsigned char *in,
   return 0;
 }
 
+void sign() {
+  int i;
+  //  crypto_onetimeauth(a, c, 131, rs);
+  for (i = 0; i < 16; ++i) {
+    // printf(",0x%02x", (unsigned int)a[i]);
+    if (i % 8 == 7)
+      printf("\n");
+  }
+}
 unsigned char rs[32] = {0xee, 0xa6, 0xa7, 0x25, 0x1c, 0x1e, 0x72, 0x91,
                         0x6d, 0x11, 0xc2, 0xcb, 0x21, 0x4d, 0x3c, 0x25,
                         0x25, 0x39, 0x12, 0x1d, 0x8e, 0x23, 0x4e, 0x65,
@@ -154,40 +169,94 @@ unsigned char c[131] = {
     0x59, 0x9b, 0x1f, 0x65, 0x4c, 0xb4, 0x5a, 0x74, 0xe3, 0x55, 0xa5};
 unsigned char a[16];
 
-unsigned int test1[17] = {
-    0x8e, 0x99, 0x3b, 0x9f, 0x48, 0x68, 0x12, 0x73, 0xc2, 0x96, 0x50, 0xba,
-    0x32, 0xfc, 0x76, 0xce, 0x48
-};
-
-unsigned int test2[17] = {
-    0x33, 0x2e, 0xa7, 0x16, 0x4d, 0x96, 0xa4,  0x47, 0x6f, 0xb8, 0xc5, 0x31,
-    0xa1, 0x18, 0x6a, 0xc0, 0xdf,
-};
-
-void sign() {
-  int i;
+unsigned char expectedResult[16] = {0xf3, 0xff, 0xc7, 0x70, 0x3f, 0x94,
+                                    0x00, 0xe5, 0x2a, 0x7d, 0xfb, 0x4b,
+                                    0x3d, 0x33, 0x05, 0xd9};
+void checkCorrectness() {
   crypto_onetimeauth(a, c, 131, rs);
-  for (i = 0; i < 16; ++i) {
-    printf(",0x%02x", (unsigned int)a[i]);
-    if (i % 8 == 7)
-      printf("\n");
+  int theSame = 0;
+  for (int i = 0; i < 16; i++) {
+    if (expectedResult[i] != a[i])
+      theSame = 1;
+  }
+  if (theSame) {
+    int i;
+    printf("They were not the same\n");
+    printf("Expected result:\n");
+    for (i = 0; i < 16; ++i) {
+      printf(",0x%02x", (unsigned int)expectedResult[i]);
+      if (i % 8 == 7)
+        printf("\n");
+    }
+    printf("Result:\n");
+    for (i = 0; i < 16; ++i) {
+      printf(",0x%02x", (unsigned int)a[i]);
+      if (i % 8 == 7)
+        printf("\n");
+    }
+  } else {
+    printf("They were the same\n");
   }
 }
 
-uint64_t dobenchmark() {
-  uint64_t oldcount, newcount;
+void test() {
+  unsigned int test1[17] = {0x2f, 0xf3, 0x24, 0x4d, 0xb9, 0xe2,
+                            0x6b, 0x53, 0x49, 0x50, 0x31, 0xcd,
+                            0x8e, 0x0a, 0x49, 0x59, 0x76};
 
+  unsigned int test2[17] = {0x10, 0xa3, 0x00, 0x2c, 0xef, 0x0f,
+                            0xb8, 0x6a, 0x54, 0xea, 0x92, 0x92,
+                            0x49, 0x79, 0x7b, 0xee, 0x1e};
+
+  mulmod(test1, test2);
+}
+
+uint64_t dobenchmark() {
+  unsigned char rs[32] = {0xee, 0xa6, 0xa7, 0x25, 0x1c, 0x1e, 0x72, 0x91,
+                          0x6d, 0x11, 0xc2, 0xcb, 0x21, 0x4d, 0x3c, 0x25,
+                          0x25, 0x39, 0x12, 0x1d, 0x8e, 0x23, 0x4e, 0x65,
+                          0x2d, 0x65, 0x1f, 0xa4, 0xc8, 0xcf, 0xf8, 0x80};
+
+  unsigned char c[131] = {
+      0x8e, 0x99, 0x3b, 0x9f, 0x48, 0x68, 0x12, 0x73, 0xc2, 0x96, 0x50, 0xba,
+      0x32, 0xfc, 0x76, 0xce, 0x48, 0x33, 0x2e, 0xa7, 0x16, 0x4d, 0x96, 0xa4,
+      0x47, 0x6f, 0xb8, 0xc5, 0x31, 0xa1, 0x18, 0x6a, 0xc0, 0xdf, 0xc1, 0x7c,
+      0x98, 0xdc, 0xe8, 0x7b, 0x4d, 0xa7, 0xf0, 0x11, 0xec, 0x48, 0xc9, 0x72,
+      0x71, 0xd2, 0xc2, 0x0f, 0x9b, 0x92, 0x8f, 0xe2, 0x27, 0x0d, 0x6f, 0xb8,
+      0x63, 0xd5, 0x17, 0x38, 0xb4, 0x8e, 0xee, 0xe3, 0x14, 0xa7, 0xcc, 0x8a,
+      0xb9, 0x32, 0x16, 0x45, 0x48, 0xe5, 0x26, 0xae, 0x90, 0x22, 0x43, 0x68,
+      0x51, 0x7a, 0xcf, 0xea, 0xbd, 0x6b, 0xb3, 0x73, 0x2b, 0xc0, 0xe9, 0xda,
+      0x99, 0x83, 0x2b, 0x61, 0xca, 0x01, 0xb6, 0xde, 0x56, 0x24, 0x4a, 0x9e,
+      0x88, 0xd5, 0xf9, 0xb3, 0x79, 0x73, 0xf6, 0x22, 0xa4, 0x3d, 0x14, 0xa6,
+      0x59, 0x9b, 0x1f, 0x65, 0x4c, 0xb4, 0x5a, 0x74, 0xe3, 0x55, 0xa5};
+  unsigned char a[16];
+
+  //  unsigned int test1[17] = {0x8e, 0x99, 0x3b, 0x9f, 0x48, 0x68,
+  //                            0x12, 0x73, 0xc2, 0x96, 0x50, 0xba,
+  //                            0x32, 0xfc, 0x76, 0xce, 0x48};
+
+  //  unsigned int test2[17] = {
+  //      0x33, 0x2e, 0xa7, 0x16, 0x4d, 0x96, 0xa4, 0x47, 0x6f,
+  //      0xb8, 0xc5, 0x31, 0xa1, 0x18, 0x6a, 0xc0, 0xdf,
+  //  };
+  uint32_t oldcount, newcount;
+  unsigned char x = 5, y = 10;
   oldcount = getcycles();
-  crypto_onetimeauth(a, c, 131, rs);
+  //  crypto_onetimeauth(a, c, 131, rs);
+  //  squeezeasm(test1);
   newcount = getcycles();
   return newcount - oldcount;
 }
 
 int main() {
+  printf("Hello helloo \n");
+  checkCorrectness();
   uint64_t cyclesTaken;
   cyclesTaken = dobenchmark();
   cyclesTaken = dobenchmark();
-  cyclesTaken = dobenchmark();
+  do {
+    cyclesTaken = dobenchmark();
+  } while (cyclesTaken == 306);
   printf("This took %llu cycles\n", cyclesTaken);
   return 0;
 }
