aadd7d2fb221f6e06eaf1711f9f9154c70c056e1
stefanberg96 <bergshm@gmail.com>
2019-11-05 15:37:47 +0100

    testing for pipeline stuff in the multiplication test

--- Programs/random tests/multest/test.c
+++ Programs/random tests/multest/test.c
@@ -5,25 +5,26 @@ extern uint32_t getcycles();
 extern uint32_t securemul(unsigned char a, unsigned char b);
 extern uint32_t add();
 extern uint32_t mul320(unsigned char a);
-   unsigned char a = 55;
-   unsigned char b = 2;
+extern unsigned int bench();
+
 uint32_t dobenchmark(){
-
-
+   unsigned char a = 10;
+   unsigned char b = 2;
    uint32_t result;
    uint32_t oldcount, newcount;
 
    oldcount = getcycles();
-   result = securemul(a,b);
+   securemul(a,b);
    newcount = getcycles();
    return newcount-oldcount;
 }
 
 int main() {
   uint32_t oldcount, newcount, x;
-  x = dobenchmark();
-  x = dobenchmark();
+  x = bench();
+  x = bench();
+  x = bench();
+  x = bench();
   printf("This took %u cycles\n",x);
-  printf("%d\n",securemul(a,b));
   return 0;
 }
