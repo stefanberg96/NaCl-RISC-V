3a245cce64d6dca333696ff28535b8d8de529369
stefanberg96 <bergshm@gmail.com>
2019-11-14 09:39:37 +0100

    radix 2.26 working in c code

---
+++ Programs/Poly1305_onetimeauth/Radix2.26/onetime_auth.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+
+extern void onetime_authloopasm(const unsigned char *in, int inlen,
+                             unsigned int *h, unsigned int *r, unsigned int *c);
+extern void addasm(unsigned int h[17], const unsigned int c[17]);
+extern void add216asm(unsigned int h[9], const unsigned int c[9]);
+extern void toradix28asm(unsigned int h[17]);
+extern void squeeze216asm(unsigned int h[9]);
+extern void mulmod216asm(unsigned int h[9], unsigned int r[9]);
+extern unsigned int* getsp();
+
+
+int crypto_onetimeauth(unsigned char *out, const unsigned char *in,
+                       unsigned long long inlen, const unsigned char *k); 
