5e3279aed068912832972f5280a4fb1f7bf3f8fa
stefanberg96 <bergshm@gmail.com>
2020-03-01 17:59:20 +0100

    improved karatsuba_2.S

--- Programs/WithMultiplication/Curve25519_scalarmult/Radix226/benchmark.c
+++ Programs/WithMultiplication/Curve25519_scalarmult/Radix226/benchmark.c
@@ -17,9 +17,9 @@
 
     void dobenchmark() {
 
-        static unsigned char g_bytes[32] = {0x8, 0x1f, 0xd, 0x75, 0x3d, 0x51, 0x94, 0xdf, 0x94, 0xee, 0x67, 0x78, 0xca, 0x23, 0x82, 0x85, 0x50, 0xda, 0x33, 0xa1, 0x18, 0x62, 0xcd, 0xeb, 0xe2, 0xbd, 0x9f, 0x86, 0x26, 0x1c, 0xc4, 0x5b};
-        static unsigned char n_bytes[32] = {0x78, 0xc4, 0xd0, 0x83, 0x9e, 0x64, 0xa5, 0xa1, 0x84, 0xed, 0x92, 0xf3, 0x82, 0x1f, 0x22, 0x43, 0xd9, 0x63, 0xdb, 0x36, 0x61, 0xf2, 0x8a, 0xf1, 0x40, 0x39, 0x42, 0xf6, 0xd6, 0x52, 0xe6, 0x6a};
-
+    
+        static unsigned char g_bytes[32] = {0x40, 0xa, 0xb7, 0xda, 0xb0, 0x54, 0x41, 0x72, 0xee, 0xc0, 0x8f, 0x53, 0x97, 0xcf, 0x5e, 0xa4, 0x6f, 0x31, 0x15, 0xa7, 0xbd, 0xbd, 0xcc, 0x28, 0xdf, 0xc4, 0xe1, 0xf5, 0xb4, 0xd, 0x4e, 0x7f};
+        static unsigned char n_bytes[32] = {0xd8, 0x9f, 0x96, 0x55, 0x11, 0xfc, 0xdd, 0xa7, 0xa8, 0x57, 0xb3, 0xbe, 0xec, 0x2c, 0xc7, 0x63, 0x42, 0x2b, 0x4c, 0x15, 0x6e, 0x1a, 0x2e, 0x39, 0xee, 0x33, 0x9b, 0x47, 0xc2, 0xc, 0xf3, 0x59};
 
         unsigned int counters[3*21];
         icachemisses();
@@ -27,7 +27,7 @@
         unsigned char q[32];
         for(int i =0;i<21;i++){
             getcycles(&counters[i*3]);
-            crypto_scalarmult(q, n_bytes, g_bytes);
+            crypto_scalarmult_asm(q, n_bytes, g_bytes);
         }
 
         printf("Cycle counts:          ");
@@ -42,48 +42,4 @@
         printf("Result: ");
         printarray(q, 32);
         printf("\n\n");
-    }
-
-    void printparams(unsigned int * a, unsigned int * b){
-
-     printf("A=");
-     printintarray(a,10);
-     printf("B=");
-     printintarray(b,10);
-    }
-
-    void printkarresult(unsigned int *r){
-     printf("R=");
-     printintarray(r,10);
-     printf("A=sum(A[i]*(2^26)^i for i in range(10))\n");
-     printf("B=sum(B[i]*(2^26)^i for i in range(10))\n");
-     printf("print(sum(R[i]*(2^26)^i for i in range(10)) == lift(mod(A*B, 2^255-19)))\n");
-    }
-
-    void printaddresult(unsigned int *r){
-
-     printf("R=");
-     printintarray(r,10);
-     printf("A=sum(A[i]*(2^26)^i for i in range(10))\n");
-     printf("B=sum(B[i]*(2^26)^i for i in range(10))\n");
-     printf("print(sum(R[i]*(2^26)^i for i in range(10)) == lift(mod(A+B, 2^255-19)))\n");
-
-    }
-
-    void printsubresult(unsigned int *r){
-
-     printf("R=");
-     printintarray(r,10);
-     printf("A=sum(A[i]*(2^26)^i for i in range(10))\n");
-     printf("B=sum(B[i]*(2^26)^i for i in range(10))\n");
-     printf("print(sum(R[i]*(2^26)^i for i in range(10)) == lift(mod(A-B, 2^255-19)))\n");
-
-    }
-
-    void printmulresult(unsigned int *r){
-       printf("R=");
-       printintarray(r,10);
-       printf("A= sum(A[i]*(2^26)^i for i in range(10))\n");
-     printf("B=sum(B[i]*(2^26)^i for i in range(10))\n");
-       printf("print(sum(R[i]*(2^26)^i for i in range(10))== lift(mod(A*121665,2^255-19)))\n");
     }
